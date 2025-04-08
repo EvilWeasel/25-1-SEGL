bf05069c26411fa52d5e2a9fb03b97177b4641aa
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-04-08 16:07:03 +0200

    added benchmark dotnet and tested runtime of linearsearch

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,3 +1,4 @@
+/*
 int[] cards = [3, 5, 7, 8, 9];
 int[] cards2 = new int[50000000];
 // 50_000_000
@@ -12,54 +13,12 @@ for (int i = 0; i < cards2.Length; i++)
 // int[] cards = {3, 5, 7, 8, 9};
 int query = 9;
 
-var result = FindCardLocationBinarySearch(cards, query);
+var result = Algorithms.FindCardLocationBinarySearch(cards, query);
 Console.WriteLine("Index von gesuchtem Element in cards: " + result);
 // Console.WriteLine(cards2[10]);
-var result2 = FindCardLocationBinarySearch(cards2, 40_942_069); // 40_942_068
+var result2 = Algorithms.FindCardLocationBinarySearch(cards2, 40_942_069); // 40_942_068
 Console.WriteLine("Index von gesuchtem Element in cards2: " + result2);
+*/
+using BenchmarkDotNet.Running;
 
-// Binary Search => O(log(n))
-int FindCardLocationBinarySearch(int[] cards, int query)
-{
-    int min = 0;
-    int max = cards.Length - 1;
-
-    while (min <= max)
-    {
-        int mid = (min + max) / 2;
-
-        if (query == cards[mid])
-        {
-            return mid;
-        }
-        else if (query < cards[mid])
-        {
-            max = mid - 1;
-        }
-        else
-        {
-            min = mid + 1;
-        }
-    }
-    return -1;
-}
-
-// Linear Search => O(n)
-int FindCardLocation(int[] cards, int query)
-{
-    /*
-    Definiere Variable "index" und initialisiere mit Wert 0
-    Wiederhole bis "index" gleich "cards" length:
-        Wenn "cards" an "index" gleich "query", dann:
-            Gebe "index" zurück
-    Inkrementiere "index" um 1
-    */
-    int index = 0;
-    while (index < cards.Length)
-    {
-        if (cards[index] == query)
-            return index;
-        index++; // index = index + 1;
-    }
-    return -1;
-}
+var summary = BenchmarkRunner.Run<Algorithms>();
